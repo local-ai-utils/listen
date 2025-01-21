e8b7e37940fbb9825ff50db8b00ef20a733a77ca
Joe Wegner <joe@wegnerdesign.com>
2025-01-21 16:34:18 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="local-ai-utils-listen",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
